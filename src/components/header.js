e9b3336459f5d1d815c4f3d1b2deac14da78e738
Kimani Kabiria <kabiriakimani@gmail.com>
2020-02-11 16:55:20 +0300

    Started on Header Section, Added top text & image

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ const Header = ({ siteTitle }) => (
 		<div className="columns headerMgn">
 			<div className="column is-one-third-desktop"></div>
 			<div className="column is-two-thirds-desktop">
-				<h1 className="is-size-1-desktop is-size-4-mobile is-navy">Hi, I am <span className="has-text-weight-bold">Alvin Kimani.</span></h1>
+				<h1 className="is-size-1-desktop is-size-4-mobile is-size-3-tablet is-navy">Hi, I am <span className="has-text-weight-bold">Alvin Kimani.</span></h1>
 				<h2 className="is-size-3-desktop is-navy">I build things that live on the internet</h2>
 			</div>
 		</div>
